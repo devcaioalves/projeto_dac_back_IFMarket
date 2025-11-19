dc10cf5d59a5f31f6f7506df328c268788e6710b
livia <liviagoncalves998@gmail.com>
2025-11-19 20:36:29 -0300

    feat(categoria): criacao da paginacao

--- src/main/java/io/github/devcaioalves/projetodacbackifmarket/repositories/CategoriaRepository.java
+++ src/main/java/io/github/devcaioalves/projetodacbackifmarket/repositories/CategoriaRepository.java
@@ -1,6 +1,7 @@
 package io.github.devcaioalves.projetodacbackifmarket.repositories;
 
 import io.github.devcaioalves.projetodacbackifmarket.entities.Categoria;
+import io.github.devcaioalves.projetodacbackifmarket.repositories.projection.CategoriaProjection;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
