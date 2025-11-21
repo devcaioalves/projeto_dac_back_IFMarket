fc4de64dbd934f8eed73c92d75f162ee0425b2f7
livia <liviagoncalves998@gmail.com>
2025-11-21 00:29:46 -0300

    feat(notificacao): criacao dos DTOs

---
+++ src/main/java/io/github/devcaioalves/projetodacbackifmarket/dto/fotoitem/FotoResponseDTO.java
@@ -0,0 +1,18 @@
+package io.github.devcaioalves.projetodacbackifmarket.dto.fotoitem;
+
+import io.github.devcaioalves.projetodacbackifmarket.entities.Item;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class FotoResponseDTO {
+
+    private Long idFoto;
+    private String caminhoArquivo;
+    private Item item;
+}
